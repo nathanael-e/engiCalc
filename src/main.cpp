1347c4558c36e7a258273dba4665bfd1d43f065b
Nathanael Eneroth <nathanael.eneroth@gmail.com>
2016-08-18 11:10:47 +0200

    Initial commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
+#include "tokenizer.hpp"
+
+using namespace engicalc;
 
 int main()
 {
+
+    Tokenizer tokenFactory;
+
+    auto tokens = tokenFactory.toTokens("2+2");
+
     return 0;
 }
